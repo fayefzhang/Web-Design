95f4fc053109586c52808e56bfdd31c43673894e
Faye Zhang <fzhang@eastsideprep.org>
2019-05-03 03:23:28 -0700

    new proj better designer

---
+++ faye_better_designer/js/home.js
@@ -0,0 +1,12 @@
+//palette (cause im a pro web designer already)
+//base "#3C85E8";
+//light "#59E1FF";
+//shadow "#29268E"
+//complimentary "#FE741C";
+
+function mouseXFollow(e) {
+    var x = e.clientX;
+    var padd = 16 + x/2000; //value between 1500 and 2000
+    //console.log(padd);
+    document.getElementById("face").style.paddingLeft = "" + padd + "%";
+}
