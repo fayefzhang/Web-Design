e997d7ea9813fba0433e28d9734b81a5a9038523
Faye Zhang <fzhang@eastsideprep.org>
2019-05-06 15:18:49 -0700

    changed back

--- faye_better_designer/js/home.js
+++ faye_better_designer/js/home.js
@@ -7,6 +7,5 @@
 function mouseXFollow(e) {
     var x = e.clientX;
     var padd = 30 + x/2000; //value between 1500 and 2000
-    //console.log(padd);
     document.getElementById("face").style.paddingLeft = "" + padd + "%";
 }
